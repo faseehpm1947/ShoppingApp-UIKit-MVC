251504e53ca64a0574d72bfeeaef9fee25a59ff1
Faseeh PM <faseeh@pixbitsolutions.com>
2025-04-15 19:13:15 +0530

    Initial commit

---
+++ HayStekAssignmentTests/HayStekAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  HayStekAssignmentTests.swift
+//  HayStekAssignmentTests
+//
+//  Created by Faseeh PM on 15/04/25.
+//
+
+import Testing
+@testable import HayStekAssignment
+
+struct HayStekAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
